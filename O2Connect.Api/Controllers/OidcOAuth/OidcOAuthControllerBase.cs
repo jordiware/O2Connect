c5c32ed36c1ca029f1c3a81c5d09c5cacfe89d7d
Jordi Vilaplana <jordiware@protonmail.com>
2026-05-28 20:16:24 +0200

    Add OIDC/OAuth base controller

---
+++ O2Connect.Api/Controllers/OidcOAuth/OidcOAuthControllerBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace O2Connect.Api.Controllers.OidcOAuth;
+
+public abstract class OidcOAuthControllerBase : ControllerBase
+{
+    public IActionResult OkJsonResponse(object? response)
+    {
+        Response.Headers.CacheControl = "no-store";
+        Response.Headers.Pragma = "no-cache";
+
+        return new JsonResult(response)
+        {
+            StatusCode = (int)HttpStatusCode.OK,
+            ContentType = "application/json"
+        };
+    }
+}
--- O2Connect.Api/Controllers/OidcOAuth/TokenController.cs
+++ O2Connect.Api/Controllers/OidcOAuth/TokenController.cs
@@ -4,13 +4,12 @@ using O2Connect.Api.Exceptions;
 using O2Connect.Api.Models;
 using O2Connect.Api.Services;
 using O2Connect.Dto.Requests;
-using System.Net;
 
 namespace O2Connect.Api.Controllers.OidcOAuth;
 
 [ApiController]
 [Route("connect/token")]
-public class TokenController : ControllerBase
+public class TokenController : OidcOAuthControllerBase
 {
     private readonly IClientAuthenticationService _clientAuthenticationService;
     private readonly ITokenRequestValidatorResolver _requestValidatorResolver;
@@ -30,39 +29,23 @@ public class TokenController : ControllerBase
     public async Task<IActionResult> Token([FromForm] TokenRequest request)
     {
         if (!Request.HasFormContentType)
-        {
             throw OAuthException.FromInvalidRequest();
-        }
-
         if (!ModelState.IsValid)
-        {
             throw OAuthException.FromInvalidRequest();
-        }
 
         var (result, authenticatedClient) = await _clientAuthenticationService
             .AuthenticateAsync(Request, request, HttpContext.RequestAborted);
 
         if (!result || authenticatedClient is null)
-        {
             throw OAuthException.FromInvalidClient();
-        }
 
         var grantType = GrantType.Parse(request.GrantType);
         var requestValidator = _requestValidatorResolver.Resolve(grantType);
-        var requestContext = await requestValidator.ValidateAsync(request,
-                                                                  authenticatedClient.Client,
-                                                                  authenticatedClient.Method,
-                                                                  HttpContext.RequestAborted);
+        var requestContext = await requestValidator
+            .ValidateAsync(request, authenticatedClient.Client, authenticatedClient.Method, HttpContext.RequestAborted);
 
         var response = await _tokenService.HandleAsync(requestContext, HttpContext.RequestAborted);
 
-        Response.Headers.CacheControl = "no-store";
-        Response.Headers.Pragma = "no-cache";
-
-        return new JsonResult(response)
-        {
-            StatusCode = (int)HttpStatusCode.OK,
-            ContentType = "application/json"
-        };
+        return OkJsonResponse(response);
     }
 }
