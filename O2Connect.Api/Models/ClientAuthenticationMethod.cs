b8e848fdbd466827e797f286c4f6a559c8187bd2
Jordi Vilaplana <jordiware@protonmail.com>
2026-05-29 13:47:36 +0200

    Strenghten smart enum types

--- O2Connect.Api/Models/ClientAuthenticationMethod.cs
+++ O2Connect.Api/Models/ClientAuthenticationMethod.cs
@@ -2,14 +2,31 @@
 
 namespace O2Connect.Api.Models;
 
-public readonly record struct ClientAuthenticationMethod(string Value)
+public readonly record struct ClientAuthenticationMethod
 {
+    public string Value { get; }
+
     public static readonly ClientAuthenticationMethod ClientSecretBasic = new("client_secret_basic");
     public static readonly ClientAuthenticationMethod ClientSecretPost = new("client_secret_post");
     public static readonly ClientAuthenticationMethod PrivateKeyJwt = new("private_key_jwt");
 
-    public static ClientAuthenticationMethod Parse(string value)
+    public static IReadOnlyCollection<ClientAuthenticationMethod> Supported { get; } =
+    [
+        ClientSecretBasic,
+        ClientSecretPost,
+        PrivateKeyJwt
+    ];
+
+    private ClientAuthenticationMethod(string value)
     {
+        Value = value;
+    }
+
+    public static ClientAuthenticationMethod Parse(string? value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw OAuthException.FromInvalidRequest();
+
         return value switch
         {
             "client_secret_basic" => ClientSecretBasic,
--- O2Connect.Api/Models/PkceMethod.cs
+++ O2Connect.Api/Models/PkceMethod.cs
@@ -2,13 +2,29 @@
 
 namespace O2Connect.Api.Models;
 
-public readonly record struct PkceMethod(string Value)
+public readonly record struct PkceMethod
 {
+    public string Value { get; }
+
     public static readonly PkceMethod Plain = new("plain");
     public static readonly PkceMethod S256 = new("S256");
 
-    public static PkceMethod Parse(string value)
+    public static IReadOnlyCollection<PkceMethod> Supported { get; } =
+    [
+        Plain,
+        S256
+    ];
+
+    private PkceMethod(string value)
     {
+        Value = value;
+    }
+
+    public static PkceMethod Parse(string? value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw OAuthException.FromInvalidRequest();
+
         return value switch
         {
             "plain" => Plain,
