d5b16746d6c163a267b0c0a94e56f4fa82d25a4f
Jordi Vilaplana <jordiware@protonmail.com>
2026-06-01 12:45:46 +0200

    InMemoryAuthorizationSessionRepository implementation

--- O2Connect.Api/Repositories/AuthorizationSessionRepository.cs
+++ O2Connect.Api/Repositories/AuthorizationSessionRepository.cs
@@ -1,4 +1,5 @@
 ﻿using O2Connect.Api.Models.Store;
+using System.Collections.Concurrent;
 
 namespace O2Connect.Api.Repositories;
 
@@ -11,18 +12,23 @@ public interface IAuthorizationSessionRepository
 
 public class InMemoryAuthorizationSessionRepository : IAuthorizationSessionRepository
 {
+    private readonly ConcurrentDictionary<string, AuthorizationSession> _sessions = new();
+
     public Task DeleteAsync(string id, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        _sessions.Remove(id, out _);
+        return Task.CompletedTask;
     }
 
     public Task<AuthorizationSession?> GetAsync(string id, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        _sessions.TryGetValue(id, out var value);
+        return Task.FromResult(value);
     }
 
     public Task StoreAsync(AuthorizationSession session, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        _sessions.TryAdd(session.Id, session);
+        return Task.CompletedTask;
     }
 }
