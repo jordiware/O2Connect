7a38dbace484f03ef384d4c861133b29584d0e8e
Jordi Vilaplana <jordiware@protonmail.com>
2026-06-04 19:43:11 +0200

    RevocationController implementation

--- O2Connect.Api/Controllers/OidcOAuth/RevocationController.cs
+++ O2Connect.Api/Controllers/OidcOAuth/RevocationController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using O2Connect.Api.Exceptions;
+using O2Connect.Api.Services;
+using O2Connect.Dto.Requests;
 
 namespace O2Connect.Api.Controllers.OidcOAuth;
 
@@ -6,9 +10,44 @@ namespace O2Connect.Api.Controllers.OidcOAuth;
 [Route("connect/revocation")]
 public class RevocationController : ControllerBase
 {
-    [HttpPost]
-    public IActionResult Revoke()
+    private readonly IRevocationService _revocationService;
+
+    public RevocationController(IRevocationService revocationService)
     {
-        return Ok("/connect/revocation endpoint");
+        _revocationService = revocationService;
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> Revoke([FromForm] RevocationRequest request)
+    {
+        var clientId = ExtractClientId(HttpContext);
+
+        if (string.IsNullOrEmpty(clientId))
+            throw OAuthException.FromInvalidClient();
+
+        await _revocationService.HandleAsync(request, clientId, HttpContext.RequestAborted);
+
+        return Ok();
+    }
+
+    private static string? ExtractClientId(HttpContext context)
+    {
+        var user = context.User;
+
+        if (user?.Identity?.IsAuthenticated != true)
+            return null;
+
+        var clientId = user.FindFirst("client_id")?.Value;
+
+        if (!string.IsNullOrWhiteSpace(clientId))
+            return clientId;
+
+        var azp = user.FindFirst("azp")?.Value;
+
+        if (!string.IsNullOrWhiteSpace(azp))
+            return azp;
+
+        return null;
     }
 }
---
+++ O2Connect.Dto/Requests/RevocationRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace O2Connect.Dto.Requests;
+
+public sealed record RevocationRequest
+{
+    public string Token { get; init; } = default!;
+    public string? TokenTypeHint { get; init; }
+}
