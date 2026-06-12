0d75580823b14d4715f8dce28e02931946d080db
Jordi Vilaplana <jordiware@protonmail.com>
2026-06-12 21:03:19 +0200

    Create empty management controllers

---
+++ O2Connect.Api/Controllers/Management/ClientsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace O2Connect.Api.Controllers.Management;
+
+[ApiController]
+[Route("management/clients")]
+public class ClientsController : ControllerBase
+{
+    private readonly ILogger<ClientsController> _logger;
+
+    public ClientsController(
+        ILogger<ClientsController> logger)
+    {
+        _logger = logger;
+    }
+}
---
+++ O2Connect.Api/Controllers/Management/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace O2Connect.Api.Controllers.Management;
+
+[ApiController]
+[Route("management/users")]
+public class UsersController : ControllerBase
+{
+    private readonly ILogger<UsersController> _logger;
+
+    public UsersController(
+        ILogger<UsersController> logger)
+    {
+        _logger = logger;
+    }
+}
