fd4d5dc02cb916c75ae2d5c267fecf57eedf9553
Jordi Vilaplana <jordiware@protonmail.com>
2026-06-15 20:40:49 +0200

    API exception types to relative URI

--- O2Connect.Api/Exceptions/ApiException.cs
+++ O2Connect.Api/Exceptions/ApiException.cs
@@ -6,27 +6,27 @@ public class ApiException : Exception
         new ApiException(StatusCodes.Status400BadRequest,
                          errorCode,
                          "Bad request",
-                         "https://api.yourdomain.com/errors/bad-request",
+                         "/errors/bad-request",
                          detail);
 
     public static ApiException NotFound(string errorCode, string detail) =>
         new ApiException(StatusCodes.Status404NotFound,
                          errorCode,
                          "Resource not found",
-                         "https://api.yourdomain.com/errors/not-found",
+                         "/errors/not-found",
                          detail);
 
     public int StatusCode { get; }
+    public string ErrorCode { get; }
     public string Title { get; }
     public string Type { get; }
-    public string ErrorCode { get; }
 
     private ApiException(int statusCode, string errorCode, string title, string type, string detail) 
         : base(detail)
     {
         StatusCode = statusCode;
+        ErrorCode = errorCode;
         Title = title;
         Type = type;
-        ErrorCode = errorCode;
     }
 }
