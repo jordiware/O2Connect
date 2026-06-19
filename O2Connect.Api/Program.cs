ba3e5b62268633a1a90db9956123f1bb04857d4d
Jordi Vilaplana <jordiware@protonmail.com>
2026-06-19 17:00:41 +0200

    Bugfix loading services

--- O2Connect.Api/Program.cs
+++ O2Connect.Api/Program.cs
@@ -79,6 +79,7 @@ builder.Services.AddSingleton<ITokenFactory, JwtTokenFactory>();
 builder.Services.AddSingleton<ISecretHasher, Pbkdf2SecretHasher>();
 builder.Services.AddSingleton<IPushedAuthorizationValidator, PushedAuthorizationValidator>();
 
+builder.Services.AddSingleton<IClientSecretValidator, ClientSecretValidator>();
 builder.Services.AddSingleton<IPaginationQueryValidator, PaginationQueryValidator>();
 builder.Services.AddSingleton<IClientsQueryValidator, ClientsQueryValidator>();
 builder.Services.AddSingleton<IUsersQueryValidator, UsersQueryValidator>();
